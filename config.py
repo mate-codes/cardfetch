010d55505143b9cff64c37ce0b4c209e135125a2
mate-codes | iMate <127528846+mate-codes@users.noreply.github.com>
2023-10-16 20:17:34 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 from colorama import *
 
-
-print(Fore.BLUE) # Text Color (BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET)
-print(Back.RESET) # Background Color (BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET)
-print(Style.NORMAL) # Text Style (DIM, NORMAL, BRIGHT, RESET_ALL)
+ascii_color = Fore.RESET
+text_info = Fore.RESET
+text_color = Fore.RESET  
+title_color = Fore.RESET
+dash_color = Fore.RESET
+print(Back.RESET)  
+print(Style.NORMAL)  
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -14,42 +17,37 @@ path = r"ascii.txt"
 ascii_art = open(path, 'r').read()
 
 
+colored_ascii_art = f"{ascii_color}{ascii_art}{Fore.RESET}"
 
-text = """           Your Name
---------------------------------
-Age: your age
-Gender: your gender
-Pronouns: your pronouns
-Email: your email
-Phone 1: your phone number 1
-Phone 2: your phone number 2
-Website: your website
-YouTube: your youtube channel
-Discord: your discord username
-Matrix: your matrix
-Lemmy: your lemmy account
-Git: your github or gitlab
-Twitch: your twitch
-TikTok: your tiktok acc
-Twitter / X: your Twitter / X acc
-Reddit: your u/username
-
-
+text = f"""{title_color}           Your Name{ascii_color}
+{dash_color}--------------------------------{ascii_color}
+{text_color}Age: {text_info}your age{ascii_color}
+{text_color}Gender: {text_info}your gender{ascii_color}
+{text_color}Pronouns: {text_info}your pronouns{ascii_color}
+{text_color}Email: {text_info}your email{ascii_color}
+{text_color}Phone 1: {text_info}your phone number 1{ascii_color}
+{text_color}Phone 2: {text_info}your phone number 2{ascii_color}
+{text_color}Website: {text_info}your website{ascii_color}
+{text_color}YouTube: {text_info}your youtube channel{ascii_color}
+{text_color}Discord: {text_info}your discord username{ascii_color}
+{text_color}Matrix: {text_info}your matrix{ascii_color}
+{text_color}Lemmy: {text_info}your lemmy account{ascii_color}
+{text_color}Git: {text_info}your github or gitlab{ascii_color}
+{text_color}Twitch: {text_info}your twitch{ascii_color}
+{text_color}TikTok: {text_info}your tiktok acc{ascii_color}
+{text_color}Twitter / X: {text_info}your Twitter / X acc{ascii_color}
+{text_color}Reddit: {text_info}your u/username{ascii_color}
 
 (Config me on config.py)
 (Config ASCII Art on ascii.txt)
 """
 
-
-ascii_lines = ascii_art.split('\n')
+ascii_lines = colored_ascii_art.split('\n')
 text_lines = text.split('\n')
 
-
 max_lines = max(len(ascii_lines), len(text_lines))
-
 
 for i in range(max_lines):
     ascii_line = ascii_lines[i] if i < len(ascii_lines) else ''
     text_line = text_lines[i] if i < len(text_lines) else ''
     print(f"{ascii_line}   {text_line}")
-
